83616b2fc7c77254061eedf9a9442816306749bc
pezzino1 <63758294+pezzino1@users.noreply.github.com>
2022-01-25 08:09:17 -0500

    Add files via upload

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Vinnie Pezzino on 1/21/22.
+//
+
+import Foundation
+class ChecklistItem: NSObject {
+ var text = ""
+ var checked = false
+}
